04f3b920002c37f1e312b307c91243ccd00adcc6
Lucia Sepulveda <luciabsep18@gmail.com>
2021-06-30 17:38:04 -0300

    add effect bottom bar

--- src/components/BottomBar/BottomBar.tsx
+++ src/components/BottomBar/BottomBar.tsx
@@ -1,10 +1,11 @@
 import {Box, Flex, Image, VStack, useColorModeValue} from "@chakra-ui/react"
 import * as React from "react"
 import {useMediaQuery} from "react-responsive"
-import {motion} from "framer-motion"
+import {motion, useMotionValue, useSpring, useTransform, MotionValue} from "framer-motion"
 
 import {Program} from "../../types/types"
 import {useCloseAllPrograms, useOpenProgram} from "../../context/hooks"
+import Icon from "../Icon/Icon"
 
 interface Props {
   programs: Program[]
@@ -15,38 +16,92 @@ const BottomBar: React.FC<Props> = ({programs}) => {
   const isPortrait = useMediaQuery({query: "(orientation: portrait)"})
   const closeAllPrograms = useCloseAllPrograms()
   const bgBar = useColorModeValue("rgba(255,255,255,0.2)", "rgba(0,0,0,0.3)")
+  const mouseX = useMotionValue<any>(null)
 
   return (
-    <Flex
-      backdropFilter="blur(1px)"
-      bg={bgBar}
-      borderRadius="2xl"
-      boxShadow="md"
-      h="75px"
-      m="auto"
-      p={2}
-      position="sticky"
-      transitionTimingFunction="ease-in-out"
-      w="fit-content"
-    >
-      {programs.map((elem) => {
-        if (elem.name !== "Wallpapers")
-          return (
-            <>
-              {!isPortrait && (
-                <VStack
-                  key={elem.name}
-                  alignSelf="center"
-                  as="button"
-                  h="65px"
-                  m="5px"
-                  w="60px"
-                  onClick={() => openProgram(elem)}
-                >
-                  <motion.div whileHover={{scale: 1.2}} whileTap={{y: -50}}>
-                    <Image alignSelf="center" h="50px" src={elem.img} w="50px" />
-                  </motion.div>
-                  {(elem.open === true || elem.minimized === true) && (
+    <Flex justifyContent="center" m="auto" position="sticky" w="100%" zIndex="sticky">
+      <Flex
+        alignItems="flex-end"
+        backdropFilter="blur(1px)"
+        bg={bgBar}
+        borderRadius="2xl"
+        borderTopLeftRadius="10px"
+        borderTopRightRadius="10px"
+        boxShadow="md"
+        height="75px"
+        p={2}
+        onMouseLeave={() => mouseX.set(null)}
+        onMouseMove={(event: React.MouseEvent) => mouseX.set(event.nativeEvent.x)}
+      >
+        {programs.map((elem) => {
+          if (elem.name !== "Wallpapers")
+            return (
+              <>
+                {!isPortrait && (
+                  <VStack key={elem.name} as="button" onClick={() => openProgram(elem)}>
+                    <Icon mouseX={mouseX} src={elem.img} />
+                    {(elem.open === true || elem.minimized === true) && (
+                      <Box
+                        bg="#01afff"
+                        borderRadius="50%"
+                        boxShadow="0 0 17px 3px #01afff,0 0 4px 2px #01afff"
+                        h="3px"
+                        w="3px"
+                      />
+                    )}
+                    {elem.open !== true && elem.minimized !== true && <Box h="3px" w="3px" />}
+                  </VStack>
+                )}
+                {isPortrait && (
+                  <VStack
+                    key={elem.name}
+                    as="button"
+                    onClick={() => {
+                      openProgram(elem), closeAllPrograms(elem)
+                    }}
+                  >
+                    <Icon mouseX={mouseX} src={elem.img} />
+                    {(elem.open === true || elem.minimized === true) && (
+                      <Box
+                        bg="#01afff"
+                        borderRadius="50%"
+                        boxShadow="0 0 17px 3px #01afff,0 0 4px 2px #01afff"
+                        h="3px"
+                        w="3px"
+                      />
+                    )}
+                    {elem.open !== true && elem.minimized !== true && <Box h="3px" w="3px" />}
+                  </VStack>
+                )}
+              </>
+            )
+          else
+            return (
+              <>
+                {!isPortrait && (elem.open === true || elem.minimized === true) && (
+                  <VStack key={elem.name} as="button" onClick={() => openProgram(elem)}>
+                    <Icon mouseX={mouseX} src={elem.img} />
+                    {(elem.open === true || elem.minimized === true) && (
+                      <Box
+                        bg="#01afff"
+                        borderRadius="50%"
+                        boxShadow="0 0 17px 3px #01afff,0 0 4px 2px #01afff"
+                        h="3px"
+                        w="3px"
+                      />
+                    )}
+                    {elem.open !== true && elem.minimized !== true && <Box h="3px" w="3px" />}
+                  </VStack>
+                )}
+                {isPortrait && (elem.open === true || elem.minimized === true) && (
+                  <VStack
+                    key={elem.name}
+                    as="button"
+                    onClick={() => {
+                      openProgram(elem), closeAllPrograms(elem)
+                    }}
+                  >
+                    <Icon mouseX={mouseX} src={elem.img} />
                     <Box
                       bg="#01afff"
                       borderRadius="50%"
@@ -54,89 +109,12 @@ const BottomBar: React.FC<Props> = ({programs}) => {
                       h="3px"
                       w="3px"
                     />
-                  )}
-                </VStack>
-              )}
-              {isPortrait && (
-                <VStack
-                  key={elem.name}
-                  alignSelf="center"
-                  as="button"
-                  h="65px"
-                  m="5px"
-                  w="60px"
-                  onClick={() => {
-                    openProgram(elem), closeAllPrograms(elem)
-                  }}
-                >
-                  <motion.div whileHover={{scale: 1.2}} whileTap={{y: -50}}>
-                    <Image alignSelf="center" h="50px" src={elem.img} w="50px" />
-                  </motion.div>
-                  {(elem.open === true || elem.minimized === true) && (
-                    <Box
-                      bg="#01afff"
-                      borderRadius="50%"
-                      boxShadow="0 0 17px 3px #01afff,0 0 4px 2px #01afff"
-                      h="3px"
-                      w="3px"
-                    />
-                  )}
-                </VStack>
-              )}
-            </>
-          )
-        else
-          return (
-            <>
-              {!isPortrait && (elem.open === true || elem.minimized === true) && (
-                <VStack
-                  key={elem.name}
-                  alignSelf="center"
-                  as="button"
-                  h="65px"
-                  m="5px"
-                  w="60px"
-                  onClick={() => openProgram(elem)}
-                >
-                  <motion.div whileHover={{scale: 1.2}} whileTap={{y: -50}}>
-                    <Image alignSelf="center" h="50px" src={elem.img} w="50px" />
-                  </motion.div>
-                  <Box
-                    bg="#01afff"
-                    borderRadius="50%"
-                    boxShadow="0 0 17px 3px #01afff,0 0 4px 2px #01afff"
-                    h="3px"
-                    w="3px"
-                  />
-                </VStack>
-              )}
-              {isPortrait && (elem.open === true || elem.minimized === true) && (
-                <VStack
-                  key={elem.name}
-                  alignSelf="center"
-                  as="button"
-                  h="65px"
-                  m="5px"
-                  w="60px"
-                  onClick={() => {
-                    openProgram(elem), closeAllPrograms(elem)
-                  }}
-                >
-                  <motion.div whileHover={{scale: 1.2}} whileTap={{y: -50}}>
-                    <Image alignSelf="center" h="50px" src={elem.img} w="50px" />
-                  </motion.div>
-                  <Box
-                    bg="#01afff"
-                    borderRadius="50%"
-                    boxShadow="0 0 17px 3px #01afff,0 0 4px 2px #01afff"
-                    h="3px"
-                    w="3px"
-                  />
-                </VStack>
-              )}
-            </>
-          )
-      })}
+                  </VStack>
+                )}
+              </>
+            )
+        })}
+      </Flex>
     </Flex>
   )
 }
